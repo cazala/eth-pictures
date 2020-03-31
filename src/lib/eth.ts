c6a4abab5ea12e5b5ce61d8f1a88d6fa8fdae521
Juan Cazala <juan@decentraland.org>
2020-03-31 20:18:56 -0300

    fix: support mobile wallets

--- src/lib/eth.ts
+++ src/lib/eth.ts
@@ -1,13 +1,18 @@
 import { Eth } from 'web3x-es/eth'
 import { Address } from 'web3x-es/address'
 import { EthPictures } from '../contracts/EthPictures'
+import { isMobile } from './mobile'
 
 export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS
-console.log('CONTRACT_ADDRESS', CONTRACT_ADDRESS)
 
 export const NO_WALLET_ERROR = 'NoWalletError'
 
 export async function mint(tokenUri: string) {
+  // patch mobile wallets
+  if (isMobile()) {
+    patchSendAsyncMethod()
+  }
+
   const eth = Eth.fromCurrentProvider()
   if (eth) {
     let accounts = await eth.getAccounts()
@@ -18,6 +23,7 @@ export async function mint(tokenUri: string) {
         throw new Error('Could not connect to wallet!')
       }
     }
+
     const address = Address.fromString(CONTRACT_ADDRESS!)
     const pictures = new EthPictures(eth, address)
     const txHash = await pictures.methods
@@ -57,4 +63,16 @@ export async function getPrice() {
     }
   }
   return usd
+}
+
+export async function patchSendAsyncMethod() {
+  // Hack for old providers that don't have a way to convert send to sendAsync
+  const provider = (window as any).ethereum
+  if (
+    provider &&
+    typeof provider.sendAsync === 'function' &&
+    provider.send !== provider.sendAsync
+  ) {
+    provider.send = provider.sendAsync
+  }
 }
