608e9fa4c6f5bf62ffc1d05747e03c9dcb1a32f1
Juan Cazala <juan@decentraland.org>
2020-03-31 19:26:49 -0300

    chore: remove serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Router } from 'react-router'
 import { createBrowserHistory } from 'history'
 import './index.css'
 import { App } from './components/App'
-import * as serviceWorker from './serviceWorker'
 import 'rc-slider/assets/index.css'
 require('dotenv').config()
 
@@ -16,8 +15,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
